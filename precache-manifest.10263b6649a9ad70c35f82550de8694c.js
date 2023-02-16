8ebb007ab5bee7ae3ad6dd3e62e502a8942a8f5b
Pranjali Ranade <ranadepranjali@gmail.com>
2023-02-16 22:11:05 +0530

    Updates

---
+++ precache-manifest.10263b6649a9ad70c35f82550de8694c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e0214f3232b0dd63774",
+    "url": "/pranjalis-app/static/css/main.ece3a275.chunk.css"
+  },
+  {
+    "revision": "1e0214f3232b0dd63774",
+    "url": "/pranjalis-app/static/js/main.1e0214f3.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/pranjalis-app/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/pranjalis-app/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "539ad140a2adb5a6ca3a",
+    "url": "/pranjalis-app/static/js/runtime~main.539ad140.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/pranjalis-app/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "5e8b371ca102b21b10a8e5f1091e4700",
+    "url": "/pranjalis-app/index.html"
+  }
+];
