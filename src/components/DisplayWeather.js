18c4395bbb3c973e3396fc74869e939a0af31b59
Pranjali Ranade <ranadepranjali@gmail.com>
2023-03-31 09:15:41 +0530

    fixed iconurl

--- src/components/DisplayWeather.js
+++ src/components/DisplayWeather.js
@@ -2,7 +2,10 @@ import React from "react";
 import "./displayweather.css";
 function DisplayWeather(props) {
   const { data } = props;
-  const iconurl = "http://openweathermap.org/img/wn/" + `${data.cod !== 404 ? data.weather[0].icon : null}` + ".png";
+  const iconurl =
+    "http://openweathermap.org/img/wn/" +
+    `${data.cod !== 404 ? data.weather[0].icon : null}` +
+    ".png";
   return (
     <div className="displayweather">
       {data.cod !== 404 ? (
