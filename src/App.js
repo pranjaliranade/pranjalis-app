3ee32ee6edda6f6867eadf3fef4d45355a9b997c
Pranjali Ranade <ranadepranjali@gmail.com>
2023-03-31 08:15:04 +0530

    weather app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import React from 'react'
 import logo from './logo.svg';
 import './App.css';
+import Weather from "./components/Weather";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello! my name is Pranjali and this is my first website.
-          Testing 123456
-          This is master and not branch2
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       
+      <Weather/>
     </div>
     
   );
